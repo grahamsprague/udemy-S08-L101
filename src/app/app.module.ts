2df108da92bd180f5c5b02e746dc6a5276db5b2c
Graham Sprague <graham.sprague@boscww-nmx2026.fios-router.home>
2018-03-26 20:42:07 -0400

    graham: initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewCourseFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/new-course-form/new-course-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormGroup, FormArray, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-course-form',
+  templateUrl: './new-course-form.component.html',
+  styleUrls: ['./new-course-form.component.css']
+})
+export class NewCourseFormComponent {
+
+  form = new FormGroup({
+    topics: new FormArray([])
+  });
+
+  addTopic(topic: HTMLInputElement) {
+    this.topics.push(new FormControl(topic.value));
+    // console.log(this.form.get('topics'));
+  }
+
+  get topics() {
+    return this.form.get('topics') as FormArray;
+  }
+
+
+
+
+}
+
